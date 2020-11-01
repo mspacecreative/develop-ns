0d5326aa95333301d08dc62fe27aa361cd021d96
Matt <matt@mspacecreative.com>
2020-11-01 12:56:27 -0400

    c

--- templates/menu.php
+++ templates/menu.php
@@ -8,23 +8,9 @@
 			</div>
 			<div class="col col-lg-4 col-md-4 col-sm-12 col-xs-12">
 				<h3 style="margin-bottom: 5px;">Share the Report</h3>
-				<?php echo do_shortcode('[addthis tool="addthis_inline_share_toolbox_s03r"]'); ?>
-				<!--
-				<?php if ( have_rows('channels', 'options') ) {
-					echo '<div class="socialMedia inline">
-							<ul>';
-					while ( have_rows('channels', 'options') ) {
-						the_row();
-						$link = get_sub_field('link', 'options');
-						$icon = get_sub_field('icon', 'options');
-						
-						echo '<li><a href="' . $link . '" target="_blank"><i class="fa ' . $icon . '"></i></a></li>';
-					}
-					echo 	'</ul>
-						 </div>';
-				}
-				-->
-				<?php 
+				
+				<?php echo do_shortcode('[addthis tool="addthis_inline_share_toolbox_s03r"]');
+				
 				$address = get_field('address', 'options');
 				if ( $address ) {
 					echo '<p>' . $address . '</p>';
