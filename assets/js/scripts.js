e5304ab71c074b9946215072541fb2a46a62d9b0
Matt <matt@mspacecreative.com>
2020-11-03 10:28:44 -0400

    c

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -20,7 +20,7 @@
 		
 		// CHANGE FADE IN DIRECTION ON IMAGE
 		if ( $('.row').hasClass('reverse') ) {
-			$(this).children('img').attr('data-aos', 'fade-right');
+			$(this).children('.col > img').attr('data-aos', 'fade-right');
 		}
 		
 		// AOS INIT
